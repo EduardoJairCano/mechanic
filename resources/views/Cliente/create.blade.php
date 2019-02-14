9f2dd1ce311c9f8b6b4c80ab54dd4f26bdbb8908
eduardojaircano <eduardojaircano@hotmail.com>
2019-02-14 17:57:32 -0600

    Primero Blade y uso de directivas

--- resources/views/Cliente/create.blade.php
+++ resources/views/Cliente/create.blade.php
@@ -1,15 +1,24 @@
-<html>
-<head>
-    <title>Cleinte</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
-</head>
-    <body>
-        <div class="container">
-            <divc class="form-group">
-                <label for="">Nombre</label>
-                <input type="text" class="form-control">
-            </divc>
-            <button type="submit" class="btn btn-primary">Guardar</button>
-        </div>
-    </body>
-</html>
+{{--
+    Para poder utilizar alguna plantilla previemente creada(heredar desde un layout), y solamente realizar render a
+    ciertas partes del codigo, para heredar cierto codigo, se emplea la directiva @extends, la cual recibe como parámetros
+    el nombre de la dirección:              @<directiva>('<direccion>')
+--}}
+@extends('Layouts.app')
+
+{{--
+    Al utilizar plantillas, y querer agregar contenido en las directivas @field, basta con implementarlas de la siguiente
+    forma:      @section('<Nombre del campo>', '<Contenido deseado>')
+--}}
+@section('title', 'Crear Cliente')
+
+{{--
+    Tambien es posible crear sections en el cual su contenido sea un formto específico de HTML
+--}}
+@section('content')
+    <div class="form-group">
+        <label for="">Nombre</label>
+        <input type="text" class="form-control">
+    </div>
+
+    <button type="submit" class="btn btn-primary">Guardar</button>
+@endsection
---
+++ resources/views/Layouts/app.blade.php
@@ -0,0 +1,17 @@
+{{--
+    Creacion de la primera plantilla de blade
+    Este archivo funcionará como archivo base para algunas otros archivos o páginas, en este tipo de archivos se podra
+        localizar ciertas directivas de Laravel, uno de las principales es @yield() en la cuale se rempleza contenido
+
+--}}
+<html>
+    <head>
+        <title> Mechanicus - @yield('title') </title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
+        <body>
+            <div class="container">
+                @yield('content')
+            </div>
+        </body>
+    </head>
+</html>
