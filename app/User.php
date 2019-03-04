b8a4295861e79e9f623cd47905d14b5eada9e6fa
Eduardo Jair Cano <jair.ci@incfile.com>
2019-03-04 15:08:43 -0600

    Creación de lógica para la validación de roles de usuario

--- app/User.php
+++ app/User.php
@@ -15,6 +15,55 @@ class User extends Authenticatable
     }
 
     /**
+     * Función que valida los permisos con los que cuenta cada rol
+     *
+     * @param object
+     * @return boolean
+     */
+    public function authorizeRoles($roles) {
+        if($this->hasAnyRole($roles)) {
+            return true;
+        }
+        /* El método abort() sirve para ejecutar errores HTTP, los cuales se enfocan en las
+            autorizaciones y accesos*/
+        abort(401, 'This action is unauthorized');
+    }
+
+    /**
+     * Función revisa un si corresponde algun tipo de arreglo a un usuario
+     *
+     * @param array
+     * @return boolean
+     */
+    public function hasAnyRole($roles) {
+        if (is_array($roles)) {
+            foreach ($roles as $role) {
+                if($this->hasRole($role)) {
+                    return true;
+                }
+            }
+        } else {
+            if($this->hasRole($roles)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Función valida si un usuario cuenta con algun rol
+     *
+     * @param object
+     * @return boolean
+     */
+    public function hasRole($role) {
+        if ($this->roles()->where('name', $role)->first()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
